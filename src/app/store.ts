969bb8741645ca91339b405cb47e9f28de80375c
Kim Rejstrom <kim.rejstrom@futurice.com>
2020-01-02 11:29:29 +0200

    setup github ci workflow

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,6 +7,13 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./rootReducer', () => {
+    const newRootReducer = require('./rootReducer').default;
+    store.replaceReducer(newRootReducer);
+  });
+}
+
 export type AppDispatch = typeof store.dispatch;
 
 export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
